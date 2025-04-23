6cbc75601967da5e871f132c078593b95c38a59e
shivamg27071999 <shivamg27071999@gmail.com>
2025-04-23 15:30:52 +0530

    23.04.2025

---
+++ index.js
@@ -0,0 +1,14 @@
+//console.log("workiing")
+//alert("10 days working")
+//confirm("do you want to continue")
+//prompt("Enter you name ")
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("demo").innerHTML = "<h1>hello</h1>"
+    function show() {
+        console.log("working")
+    }
+    show();
+})
+
+
+
